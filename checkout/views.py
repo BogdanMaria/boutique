d9ca40d4ff488caf3a0db438dd89aa5bb70f59dc
Bogdan Maria <maria_costinel@yahoo.com>
2024-04-27 15:07:59 +0000

    Added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -12,5 +12,7 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51PACJrHm86RGK6pp2Ii7iXbHUJACJXME68qHtgpDyL2nQnCjptkuLj3QZSFhdFa97zWDaNnHsWclmGkkQjD8kPSK00BNo2YmDX',
+        'client_secret': 'test client secret',
     }
     return render(request, template, context)
